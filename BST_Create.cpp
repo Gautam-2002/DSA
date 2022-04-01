17ad9046fa1996f72fb0a325d4bceb9037e34059
Gautam-2002 <gautam2sethi@gmail.com>
2022-04-01 23:39:13 +0530

    01/3

--- BST_Create.cpp
+++ BST_Create.cpp
@@ -16,7 +16,8 @@ public:
     }
 };
 
-Node* insertIntoBST(Node* &root,int d){
+Node *insertIntoBST(Node *&root, int d)
+{
     if (root == NULL)
     {
         root = new Node(d);
@@ -24,31 +25,30 @@ Node* insertIntoBST(Node* &root,int d){
     }
     if (d > root->data)
     {
-        root->right = insertIntoBST(root->right,d);
+        root->right = insertIntoBST(root->right, d);
     }
     else
     {
-        root->left = insertIntoBST(root->left,d);
+        root->left = insertIntoBST(root->left, d);
     }
     return root;
-    
 }
 
-void takeInput(Node* &root)
+void takeInput(Node *&root)
 {
     int data;
-    cin>>data;
+    cin >> data;
 
-    while (data!=-1)
+    while (data != -1)
     {
-        insertIntoBST(root,data);
-        cin>>data;
+        insertIntoBST(root, data);
+        cin >> data;
     }
-    
 }
 
 void levelOrderTraversal(Node *root)
-{   cout<<"Level Order Traversel:"<<endl;
+{
+    cout << "Level Order Traversel:" << endl;
     queue<Node *> q;
     q.push(root);
     q.push(NULL);
@@ -80,13 +80,55 @@ void levelOrderTraversal(Node *root)
     }
 }
 
+void preorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void postorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
 int main()
 {
     Node *root = NULL;
 
     cout << "Enter Data to create BST" << endl;
     takeInput(root);
-    cout << ":Printing BST (Level Order):"<<endl;
+    cout << "Printing BST (Level Order):" << endl;
     levelOrderTraversal(root);
+    cout <<"Printing BST (Pre Order):" << endl;
+    preorder(root);
+    cout<< endl << "Printing BST (In Order):" << endl;
+    inorder(root);
+    cout<< endl << "Printing BST (Post Order):" << endl;
+    postorder(root);
     return 0;
 }
