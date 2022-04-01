2cd1205b7d930672e593d53da12d0c1945f804fb
Gautam-2002 <gautam2sethi@gmail.com>
2022-04-01 23:29:07 +0530

    01/4

---
+++ BST_Create.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* insertIntoBST(Node* &root,int d){
+    if (root == NULL)
+    {
+        root = new Node(d);
+        return root;
+    }
+    if (d > root->data)
+    {
+        root->right = insertIntoBST(root->right,d);
+    }
+    else
+    {
+        root->left = insertIntoBST(root->left,d);
+    }
+    return root;
+    
+}
+
+void takeInput(Node* &root)
+{
+    int data;
+    cin>>data;
+
+    while (data!=-1)
+    {
+        insertIntoBST(root,data);
+        cin>>data;
+    }
+    
+}
+
+void levelOrderTraversal(Node *root)
+{   cout<<"Level Order Traversel:"<<endl;
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+
+        q.pop();
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+int main()
+{
+    Node *root = NULL;
+
+    cout << "Enter Data to create BST" << endl;
+    takeInput(root);
+    cout << ":Printing BST (Level Order):"<<endl;
+    levelOrderTraversal(root);
+    return 0;
+}
