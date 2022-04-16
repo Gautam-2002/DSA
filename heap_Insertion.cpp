721329aebe443081a563e9fe2083d1e30db2d2f7
Gautam-2002 <gautam2sethi@gmail.com>
2022-04-16 10:33:33 +0530

    insert,print

--- heap_Insertion.cpp
+++ heap_Insertion.cpp
@@ -6,6 +6,37 @@ class heap
 public:
     int arr[100];
     int size = 0;
+
+    void insert(int val){
+        size = size + 1;
+        int index = size;
+        arr[index]=val;
+
+        while (index > 1)
+        {
+            int parent = index/2;
+
+            if (arr[parent]<arr[index])
+            {
+                swap(arr[parent],arr[index]);
+                index = parent;
+            }
+            else{
+                return;
+            }
+            
+        }
+        
+    }
+
+    void print(){
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        
+    }
 };
 
 int main(){
